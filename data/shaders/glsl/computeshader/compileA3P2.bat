c34b1cdd6fb312d67812b4ab02c8ec93fcd3134d
owenwensong <w.huang@digipen.edu>
2022-11-09 22:17:13 +0800

    windows renaming reupload fix

--- data/shaders/glsl/computeshader/compileA3P2.bat
+++ data/shaders/glsl/computeshader/compileA3P2.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 %VULKAN_SDK%/Bin/glslc.exe histogram.comp -o histogram.comp.spv
-%VULKAN_SDK%/Bin/glslc.exe cdfScan.comp -o cdfScan.comp.spv
+%VULKAN_SDK%/Bin/glslc.exe cdfScan.comp -o cdfscan.comp.spv
 %VULKAN_SDK%/Bin/glslc.exe applyhisto.comp -o applyhisto.comp.spv
 @ECHO OFF
 pause
